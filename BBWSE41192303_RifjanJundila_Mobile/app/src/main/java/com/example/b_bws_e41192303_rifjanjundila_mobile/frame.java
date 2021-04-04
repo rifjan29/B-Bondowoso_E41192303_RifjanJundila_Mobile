c197281650583d28dff577bf170d5965945e388c
iphang190909 <iphang.admaja@gmail.com>
2021-04-04 16:24:01 +0700

    Tugas 1
    RifjanJundila

---
+++ BBWSE41192303_RifjanJundila_Mobile/app/src/main/java/com/example/b_bws_e41192303_rifjanjundila_mobile/frame.java
@@ -0,0 +1,14 @@
+package com.example.b_bws_e41192303_rifjanjundila_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+    }
+}
